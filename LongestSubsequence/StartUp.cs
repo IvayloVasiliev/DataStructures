79918a47f45cc59c337ac966d1f3971412eaf623
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-16 15:30:21 +0200

    Longest Subsequence

---
+++ LongestSubsequence/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestSubsequence
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            int maxNumber = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int counter = 1;
+                for (int j = i+1; j < numbers.Count; j++)
+                {
+                    if (numbers[j] == numbers[i])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (counter > maxCount)
+                {
+                    maxNumber = numbers[i];
+                    maxCount = counter;
+                }
+            }
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                Console.Write(maxNumber + " ");
+            }
+        }
+    }
+}

46a15cfffcb0f958673bb3b20b916e30fb92d35a
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-16 15:00:54 +0200

    Add project files.

---
+++ DistanceInLabyrinth/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DistanceInLabyrinth
+{
+    public class Cell
+    {
+        public Cell( int row, int col, bool isVisited, int moves)
+        {
+            this.Row = row;
+            this.Col = col;
+            this.IsVisited = isVisited;
+            this.Moves = moves;
+        }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public bool IsVisited { get; set; }
+
+        public int Moves { get; set; }
+    }
+}

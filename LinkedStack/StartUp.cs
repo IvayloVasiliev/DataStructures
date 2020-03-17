03bb7a2c0c5a8328eeb2c0fd36327ff5f7c36ee4
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-17 13:43:04 +0200

    Linked Stack Implementation

---
+++ LinkedStack/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedStack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            LinkedStack<int> stack = new LinkedStack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            stack.Pop();
+
+            Console.WriteLine(string.Join(", ", stack.ToArray()));
+        }
+    }
+}

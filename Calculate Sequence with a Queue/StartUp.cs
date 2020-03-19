95ac8ae24e3af6769a0f0147fe8a9a537b542e92
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-19 17:25:06 +0200

    Calculate Sequence with a Queue

---
+++ Calculate Sequence with a Queue/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Calculate_Sequence_with_a_Queue
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(num);
+
+            int index = 0;
+            int[] arr = new int[50];
+
+            while (true)
+            {
+                int element = queue.Dequeue();
+
+                arr[index] = element;
+                index++;
+
+                if (index == 50)
+                {
+                    break;
+                }
+
+                queue.Enqueue(element + 1);
+                queue.Enqueue(2* element +1);
+                queue.Enqueue(element + 2);
+            }
+
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}

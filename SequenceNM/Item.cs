e77456815f9da2fcb39fae283ec8eee9dff72c84
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-20 17:19:46 +0200

    Sequence N -> M  program that finds the shortest sequence of operations

---
+++ SequenceNM/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequenceNM
+{
+    public class Item
+    {
+        public int value;
+        public Item prevItem;
+
+        public Item()
+        {
+
+        }
+
+        public Item(int value, Item prevItem)
+        {
+            this.value = value;
+            this.prevItem = prevItem;
+        }
+    }
+}
---
+++ SequenceNM/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SequenceNM
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //a program that finds the shortest sequence of operations 
+
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            if (startNum > endNum)
+            {
+                Console.WriteLine("no solution");
+            }
+
+            Queue<Item> queue = new Queue<Item>();
+
+            Item startItem = new Item();
+
+            startItem.value = startNum;
+
+            queue.Enqueue(startItem);
+
+            while (true)
+            {
+                Item element = queue.Dequeue();
+
+                if (element.value == endNum)
+                {
+                    while (element != null)
+                    {
+                        Console.WriteLine(element.value);
+                        element = element.prevItem;
+                    }
+                    return;
+                }
+
+                queue.Enqueue(new Item(element.value + 1, element));
+                queue.Enqueue(new Item(element.value + 2, element));
+                queue.Enqueue(new Item(element.value * 2, element));
+            }
+
+
+        }
+    }
+}

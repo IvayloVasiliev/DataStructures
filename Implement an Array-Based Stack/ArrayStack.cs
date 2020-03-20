5a9f768a70e4bc48e7baf37e7dad2f54c0abd7d6
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-03-20 16:29:49 +0200

    Array based Stack Implementation

---
+++ Implement an Array-Based Stack/ArrayStack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implement_an_Array_Based_Stack
+{
+    public class ArrayStack<T> 
+    {
+        private const int InitialCapacity = 4;
+
+        private T[] elements;
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public ArrayStack()
+        {
+            this.elements = new T[InitialCapacity];
+            this.Count = 0;
+        }
+
+        public void Push(T element)
+        {
+            int index = this.Count;
+            this.elements[index] = element;
+
+            this.Count++;
+
+            if (this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
+
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException("Stack is empty!");
+            }
+
+            int index = this.Count - 1;
+
+            T element = this.elements[index];
+
+            this.Count--;
+
+            return element;
+        }
+
+        public T[] ToArray()
+        {
+            T[] toReturn = new T[Count];
+            int index = 0;
+
+            for (int i = toReturn.Length -1; i >= 0; i--)
+            {
+                toReturn[index++] = this.elements[i];
+            }
+
+            return toReturn;
+        }
+
+        private void Grow()
+        {
+            T[] newArray = new T[2 * this.Count];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                newArray[i] = this.elements[i];
+            }
+            elements = newArray;
+        }
+
+       
+    }
+}
---
+++ Implement an Array-Based Stack/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Implement_an_Array_Based_Stack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            
+
+            Console.WriteLine(string.Join(", ", stack.ToArray()));
+
+        }
+    }
+}
